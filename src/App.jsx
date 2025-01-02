3fa40ad1ceea5bad0ac1d2c5163e6ab100cbc124
Erik Akerman <erikakerman@gmail.com>
2025-01-02 15:24:44 +0100

    Initial commit: Project setup with Vite, React, Tailwind, and shadcn/ui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="p-4">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
